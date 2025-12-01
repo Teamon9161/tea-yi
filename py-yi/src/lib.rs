891a5f29012a4adbbcef5d6888fce4763aa5c172
Teamon9161 <teamon9161@163.com>
2025-12-01 23:31:25 +0800

    1

--- py-yi/src/lib.rs
+++ py-yi/src/lib.rs
@@ -2,9 +2,9 @@ use pyo3::prelude::*;
 use pyo3_polars::derive::polars_expr;
 use pyo3_polars::export::polars_core::prelude::*;
 use pyo3_polars::export::polars_error::PolarsResult;
-use pyo3_polars::types::*;
 use serde::Deserialize;
 use tea_yi::Hexagram;
+use tevec::prelude::Vec1View;
 
 macro_rules! auto_cast {
     // for one expression
@@ -33,15 +33,28 @@ pub struct YiParams {
 }
 
 #[polars_expr(output_type=String)]
-fn evaluators_future_dirty_price(inputs: &[Series], kwargs: YiParams) -> PolarsResult<Series> {
+fn into_hexagram(inputs: &[Series], kwargs: YiParams) -> PolarsResult<Series> {
     let series = auto_cast!(Boolean(&inputs[0]));
-    series.bool()?;
-    todo!();
+    let out: Vec<_> = series
+        .bool()?
+        .rolling_custom(
+            6,
+            |s| {
+                let h = if !kwargs.rev {
+                    Hexagram::from_iter(s.iter().map(|b| b.unwrap()))
+                } else {
+                    Hexagram::from_iter(s.iter().rev().map(|b| b.unwrap()))
+                };
+                format!("{}", h.name())
+            },
+            None,
+        )
+        .unwrap();
+    Ok(StringChunked::from_iter_values("".into(), out.into_iter()).into_series())
 }
 
 /// Python module initializer.
 #[pymodule]
-fn py_yi(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
-    // m.add_function(wrap_pyfunction!(hexagram_info, m)?)?;
+fn py_yi(_py: Python<'_>, _m: &Bound<'_, PyModule>) -> PyResult<()> {
     Ok(())
 }
